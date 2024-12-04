39ccbf7974058626af8de129bb7539b95c229287
mmdmthr <muhhari@gmail.com>
2024-12-04 11:31:04 +0700

    2 test: add resource view test case

--- tests/Feature/StaffTest.php
+++ tests/Feature/StaffTest.php
@@ -11,13 +11,46 @@ class StaffTest extends TestCase
 {
     Use RefreshDatabase;
 
-    public function test_view_staff_page_is_displayed()
+    public function test_view_all_staff_page_is_displayed()
     {
         $user = User::factory()->create();
 
         $response = $this
             ->actingAs($user)
             ->get('/staff');
+        
+        $response->assertOk();
+    }
+
+    public function test_create_new_staff_page_is_displayed()
+    {
+        $user = User::factory()->create();
+
+        $response = $this
+            ->actingAs($user)
+            ->get('/staff/create');
+        
+        $response->assertOk();
+    }
+
+    public function test_edit_staff_page_is_displayed()
+    {
+        $user = User::factory()->create();
+
+        $response = $this
+            ->actingAs($user)
+            ->get("/staff/$user->id/edit");
+        
+        $response->assertOk();
+    }
+
+    public function test_view_single_staff_page_is_displayed()
+    {
+        $user = User::factory()->create();
+
+        $response = $this
+            ->actingAs($user)
+            ->get("/staff/$user->id");
         
         $response->assertOk();
     }
